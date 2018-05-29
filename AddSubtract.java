b093cbf5886eebc196329442f5c437b519242a71
PElanchezhiyan <elanche97@gmail.com>
2018-05-29 11:32:47 +0530

    Simple AddSubtract and TypeCasting program

---
+++ AddSubtract.java
@@ -0,0 +1,16 @@
+public class AddSubtract{
+
+   public static void main(String s[]){
+    System.out.println("Output value is");
+    
+     byte one = 80;
+
+     byte two = 50;
+
+     byte three = 20;
+     
+     int four = (one + two) - three;
+
+    System.out.println(four);
+  }  
+}
---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+public class TypeCasting{
+
+   public static void main(String s[]){
+    System.out.println("Output value is");
+    
+     byte one = 80;
+
+     byte two = 20;
+     
+    byte three = (byte)(one + two);
+
+    System.out.println(three);
+  }  
+}
